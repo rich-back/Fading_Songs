69836de778e33366001ae7f868a782eb4c576087
rich-back <backhouse.richard@gmail.com>
2023-04-12 00:47:18 +0100

    🐛 fixed bird animation bug

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,45 +1,39 @@
 import React from "react";
 
-const Title = ({ handleSelectChange, rarestbirds }) => {
+function Title({ handleSelectChange, rarestbirds }) {
+
   return (
     <div className="container">
-      <h1 className="title">Listen to the Rarest Birds in the World</h1>
+      <h1 className="title">Listen to the Rarest Birds in the World...</h1>
 
-      <div class="bird-container bird-container--one">
-        <div class="bird bird--one"></div>
+      <div className="bird-container bird-container--one">
+        <div className="bird bird--one"></div>
       </div>
 
-      <div class="bird-container bird-container--two">
-        <div class="bird bird--two"></div>
+      <div className="bird-container bird-container--two">
+        <div className="bird bird--two"></div>
       </div>
 
-      <div class="bird-container bird-container--three">
-        <div class="bird bird--three"></div>
+      <div className="bird-container bird-container--three">
+        <div className="bird bird--three"></div>
       </div>
 
-      <div class="bird-container bird-container--four">
-        <div class="bird bird--four"></div>
+      <div className="bird-container bird-container--four">
+        <div className="bird bird--four"></div>
       </div>
 
-      <div>
-        <select className="selector" onChange={handleSelectChange}>
+      <div className="selector-container">
+        <select className="selector" onChange={handleSelectChange} name={"Select a bird"}>
           {rarestbirds.map((rarestbird) => {
             return (
-              <option key={rarestbird.name} value={rarestbird.url}>
+              <option defaultValue={'sds'} key={rarestbird.name} value={rarestbird.url}>
                 {rarestbird.name}
               </option>
             );
           })}
         </select>
+
       </div>
-
-      {/* <div>
-        Image goes here...
-        {rarestbirds.map(rarestbird => {
-            return <img src={rarestbird.img}/>
-        })}
-
-    </div> */}
     </div>
   );
 };
