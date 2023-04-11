7b291cef960fe3836ddd6b8838dfe11866f80768
rich-back <backhouse.richard@gmail.com>
2023-04-12 00:43:13 +0100

    ✨ new Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer">
+    <p>
+      Click on a
+      <img
+        className="bird-icon"
+        src="https://cdn-icons-png.flaticon.com/512/8845/8845053.png"
+        alt="bird marker icon"
+      ></img>
+      to listen to a recording...{" "}
+    </p>
+  </div>
+  )
+}
+
+export default Footer
